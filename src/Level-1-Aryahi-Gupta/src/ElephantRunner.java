10ef940e9c63c74de2fa78c571b3ad4275da40b4
Lucas Baizer <titaniumsapphire32@gmail.com>
2017-07-14 11:57:51 -0700

    last day of Summer Slam 7/17/17

--- src/Level-1-Aryahi-Gupta/src/ElephantRunner.java
+++
@@ -1,9 +0,0 @@
-
-public class ElephantRunner {
-	public static void main (String [] args){
-		Elephant elephant1 = new Elephant("yellow");
-		Elephant elephant2 = new Elephant("gray");
-		
-		elephant1.trumpet();
-	}
-}
