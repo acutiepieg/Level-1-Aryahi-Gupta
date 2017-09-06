016a1299525b9659ac10f651695c5b5a796bb22e
JackChine27 <league@WTS14.attlocal.net>
2017-09-06 16:57:39 -0700

    9/6/17

--- src/ChuckleClicker.java
+++ src/ChuckleClicker.java
@@ -1,28 +1,72 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
-public static void main(String[] args) {
+import org.teachingextensions.logo.Colors;
+
+public class ChuckleClicker implements ActionListener {
+	JButton joke = new JButton();
+	JButton punch = new JButton();
+	JLabel label = new JLabel();
+	JFrame frame = new JFrame();
+	
+int idx = 0;
+	String[] jokes = { "How do you make an octopus laugh?", "Joke 2", "Joke 3"};
+	String[] punchlines = { "With ten-tickles", "Punchline 2", "Punchline 3"};
+	public static void main(String[] args) {
 	new ChuckleClicker().makeButtons();
 	
 }
 
 public void makeButtons(){
-	JFrame frame = new JFrame();
+	
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setVisible(true);
 	JPanel panel1 = new JPanel();
 	frame.add(panel1);
-	JButton joke = new JButton();
-	JButton punchline = new JButton();
 	panel1.add(joke);
-	panel1.add(punchline);
+	panel1.add(punch);
+	panel1.setBackground(Colors.Blues.CadetBlue);
 	JPanel panel2 = new JPanel();
 	frame.add(panel2);
-	JLabel label = new JLabel();
+	panel2.setBackground(Colors.Blues.CornflowerBlue);
 	panel2.add(label);
+	frame.setSize(700, 500);
+	frame.setLayout(new GridLayout (2,1));
+	joke.setText("Joke");
+	punch.setText("Punchline");
 	frame.pack();
+	frame.setVisible(true);
+	
+	joke.addActionListener(this);
+	punch.addActionListener(this);
+	punch.setEnabled(false);
+	
+	
+	}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(idx == 3) {
+		idx = 0;
+	}
+	
+	if(e.getSource() == joke) {
+		label.setText(jokes[idx]);
+		frame.pack();
+	punch.setEnabled(true);
+	}
+	
+	if(e.getSource() == punch) {
+		label.setText(punchlines[idx]);
+		idx ++;
+		punch.setEnabled(false);
+		frame.pack();
+	}
 }
 }
